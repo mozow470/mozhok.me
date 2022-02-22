86aa4c0d1b2dd6750c531825f4d9c2ab9b6b2184
YutoAsano <ya2020sfc@keio.jp>
2022-02-22 22:10:33 +0900

    Revert "done para"
    This reverts commit 3ee3975ec51b3428affb04a3671db2f6b75e4322.

--- components/mayor/webs.vue
+++ components/mayor/webs.vue
@@ -3,8 +3,8 @@
         <div v-for="(v,k) in data" :key="k">
             <a :href="v.properties.URL.url">
                 <table class="border-collapse w-full mt-5 mb-5 ">
-                    <tr class="border"><td class="pl-4 pr-4">{{v.properties.コメント.rich_text[0].plain_text}}</td></tr>
-                    <tr class="border"><td class=" pl-4 pr-4">{{v.properties.タイトル.title[0].plain_text}}</td></tr>
+                    <tr class="border "><td class="pl-4 pr-4">{{v.properties.コメント.rich_text[0].plain_text}}</td></tr>
+                    <tr class="border "><td class=" pl-4 pr-4">{{v.properties.タイトル.title[0].plain_text}}</td></tr>
                 </table>
             </a>
         </div>
