f2b595317f452b63e4250c68d587945cd01da72e
YutoAsano <ya2020sfc@keio.jp>
2022-02-22 22:09:12 +0900

    Revert "add title to diary"
    This reverts commit d061ae79518667951ff70ee5e2701a9548153c04.

--- pages/mayor/diary/[id].vue
+++ pages/mayor/diary/[id].vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <h1 class="text-3xl text-center font-corben">{{addSlash(route.params.id)}}</h1>
         <Commons :contens="data"/>
     </div>
 </template>
@@ -8,17 +7,6 @@
 <script setup>
 import Commons from '~~/components/commons.vue';
     const route=useRoute();
-    let url=ref('/api/diarySearch/'+route.params.id) ;
-    function addSlash(text){
-        const a=text[0];
-        const b=text[1];
-        const c=text[2];
-        const d=text[3];
-        const e=text[4];
-        const f=text[5];
-        const g=text[6];
-        const h=text[7];
-        return a+b+c+d+'/'+e+f+'/'+g+h;
-    }
+    const url='/api/diarySearch/'+route.params.id;
     const { data } = await useFetch(url);
 </script>
