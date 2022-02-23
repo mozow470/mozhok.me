a89c756328b890ce995836ca178fac664ff24f63
Riku Mochizuki <mozuku.470@gmail.com>
2022-02-23 14:37:41 +0900

    ADD; netlify configure

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,8 +5,8 @@ export default defineNuxtConfig({
     // googleAnalytics: {
     //   id: "G-4SQKPQK7X0"
     // },
-  ssr: false,
-  // target: 'static',
+  // ssr: false,
+  target: 'static',
     privateRuntimeConfig: {
       diaryURL:'https://api.notion.com/v1/databases/ad023003677c4d29bd8c75872c154b68/query',
       token:'secret_T7wfS6SFsdyGnMBEPeDH19XEywx6UKrrrTf6f8VPx4n',
@@ -31,11 +31,6 @@ export default defineNuxtConfig({
       }
     },
   nitro: {
-    preset: 'server',
-    host: '0.0.0.0', // default: localhost,
-    port: 7777,
   },
-    
-    
-    
+
 })
